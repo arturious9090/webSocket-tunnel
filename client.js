7d0adad6bfcce6b3ff02af2251b3295bb4149ce8
arturious9090 <sefer07artur@gmail.com>
2026-07-23 21:21:55 +0200

    conect to server fun

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
-import http, { WebSocket } from 'http'
+import http from 'http'
 import { URL } from 'url'
+import WebSocket from 'ws'
 
 const PORT = 8000
 const WEB_SOCKET_URL = 'ws://localhost:8080'
@@ -46,23 +47,43 @@ async function request(url, method, headers, body) {
     })
 }
 
-async function main() {
-    const socket = new WebSocket(WEB_SOCKET_URL)
-    const body = new Buffer.from('hello world!!')
-    const url = new URL('http://localhost:8000')
-    const headers = new Headers()
+async function WsConnect(retryTime = 50) {
+    const ws = new WebSocket(WEB_SOCKET_URL)
 
+    return new Promise((resolve, reject) => {
+        ws.once('open', () => {
+            console.log('connected to', WEB_SOCKET_URL)
+            resolve(ws)
+        }).once('error', (error) => {
+            if (error.name === 'AggregateError') {
+                console.log('fail to connect next attempt in', retryTime * 2 / 1000, 's')
+                if (retryTime > 100000) {
+                    console.log('conection error')
+                    process.exit(1)
+                }
+                setTimeout(WsConnect, retryTime, retryTime * 2)
+            } else {
+                console.log(error)
+            }
+        })
+    })
+}
+
+
+async function main() {
+    const socket = await WsConnect()
+
+    const url = new URL('http://localhost:8000')
+    
+    const body = new Buffer.from('hello world!!')
+    const headers = new Headers()
     headers.append('Content-Length', Buffer.byteLength(body))
 
-    socket.addEventListener('open', event => {
-        console.log('conected')
-    })
-
-    socket.addEventListener('message', (ev) => {
-        const rdata = JSON.parse(ev.data)
+    socket.on('message', (data) => {
+        const rdata = JSON.parse(data)
         const method = rdata.method
         const url = rdata.url
-        const headers =  new Headers()
+        const headers = new Headers()
         console.log(url)
         for (const h in rdata.headers) {
             headers.append(h, rdata.headers[h])
@@ -85,4 +106,4 @@ async function main() {
 
 }
 
-await main()
+main()

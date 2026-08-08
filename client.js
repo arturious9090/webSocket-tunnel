b8c4316c5ecd2c38f68255a9da37a0c056e12167
arturious9090 <sefer07artur@gmail.com>
2026-08-08 14:07:12 +0200

    websocket client reconnect wraper
    websocket client reconnect wraper

--- client.js
+++ client.js
@@ -1,85 +1,124 @@
+import { log } from 'console'
 import http from 'http'
 import { URL } from 'url'
 import WebSocket from 'ws'
 
 const PORT = 8000
 const WEB_SOCKET_URL = 'ws://localhost:8080'
+const HTTP_SERVER_HOST = 'http://localhost:8000'
 
-async function request(url, method, headers, body) {
-    return new Promise((resolve, reject) => {
-        const req = http.request(url, { method }, (res) => {
-            let data = ''
-            res.on('data', (chunk) => {
-                data += chunk
-            })
-            res.on('end', () => {
-                resolve(data)
-            })
-        }).on('error', (err) => {
-            reject(err)
-        })
-        req.setHeaders(headers)
-        req.end(body)
-    })
-}
-
-async function WsConnect(retryTime = 50) {
-    const ws = new WebSocket(WEB_SOCKET_URL)
-
-    return new Promise((resolve, reject) => {
-        ws.once('open', () => {
-            console.log('connected to', WEB_SOCKET_URL)
-            resolve(ws)
-        }).once('error', (error) => {
-            if (error.name === 'AggregateError') {
-                console.log('fail to connect next attempt in', retryTime * 2 / 1000, 's')
-                if (retryTime > 100000) {
-                    console.log('conection error')
-                    process.exit(1)
+class WebSockerReconnectWrapper {
+    constructor(address, protocols, options, retryTime) {
+        this._ws = undefined
+        this._activeListeners = { on: new Map(), once: new Map() }
+        this.address = address
+        this.protocols = protocols
+        this.options = options
+        this.retryTime = retryTime || 500
+        this.connect()
+        this._ready
+        this._maxRetrys = 50
+        this._retryCount = 0
+    }
+    connect() {
+        const ws = new WebSocket(this.address, this.protocols, this.options)
+        this._ready = new Promise((resolve) => {
+            ws.once('open', () => {
+                for (const [eventName, events] of this._activeListeners.on) {
+                    for (const e of events) {
+                        ws.on(eventName, e) // Сделать коректную обработку одноразовых событий 
+                    }
                 }
-                setTimeout(WsConnect, retryTime, retryTime * 2)
-            } else {
-                console.log(error)
-            }
+                for (const [eventName, events] of this._activeListeners.once) {
+                    for (const e of events) {
+                        ws.once(eventName, e) // Сделать коректную обработку одноразовых событий 
+                    }
+                }
+                log('conected')
+                this._retryCount = 0
+                this._ws = ws
+                resolve()
+            })
         })
-    })
-}
+        ws.once('error', (error) => {
+            if (error.name !== 'AggregateError') {
+                throw error
+            }
+        }).once('close', (code, reson) => {
+            if (this._retryCount >= this._maxRetrys) {
+                throw new Error('connection error')
+            }
+            this._retryCount += 1
+            setTimeout(() => this.connect(), this.retryTime)
+        })
+    }
 
+    async on(event, handler) {
+        await this._ready
+        try {
+            this._activeListeners.on.get(event).push(handler)
+        } catch (error) {
+            this._activeListeners.on.set(event, [handler])
+        }
+        this._ws.on(event, handler)
+    }
+
+    async once(event, handler) {
+        await this._ready
+        try {
+            this._activeListeners.once.get(event).push(handler)
+        } catch (error) {
+            this._activeListeners.once.set(event, [handler])
+        }
+        this._ws.once(event, handler)
+    }
+
+
+
+    async send(data) {
+        await this._ready
+        this._ws.send(data)
+    }
+}
 
 async function main() {
-    const socket = await WsConnect()
 
-    const url = new URL('http://localhost:8000')
-    
-    const body = new Buffer.from('hello world!!')
-    const headers = new Headers()
-    headers.append('Content-Length', Buffer.byteLength(body))
-
-    socket.on('message', (data) => {
-        const rdata = JSON.parse(data)
-        const method = rdata.method
-        const url = rdata.url
-        const headers = new Headers()
-        console.log(url)
-        for (const h in rdata.headers) {
-            headers.append(h, rdata.headers[h])
-        }
-        const body = rdata.body
-        const req = http.request('http://localhost:8000' + url, { method }, (res) => {
-            let data = ''
-            res.on('data', (chunk) => {
-                data += chunk
-            })
-            res.on('end', () => {
-                socket.send(data)
-            })
-        }).on('error', (err) => {
-            console.log(err)
-        })
-        req.setHeaders(headers)
-        req.end(body)
+    const ws = new WebSockerReconnectWrapper(WEB_SOCKET_URL)
+    ws.on('message', (data) => {
+        const httpRequest = JSON.parse(data.toString())
+        log(httpRequest)
     })
 
 }
 
+
+// const url = new URL('http://localhost:8000')
+// const body = new Buffer.from('hello world!!')
+// const headers = new Headers()
+// headers.append('Content-Length', Buffer.byteLength(body))
+
+// socket.on('message', (data) => {
+//     const rdata = JSON.parse(data)
+//     const method = rdata.method
+//     const url = rdata.url
+//     const headers = new Headers()
+//     console.log(url)
+//     for (const h in rdata.headers) {
+//         headers.append(h, rdata.headers[h])
+//     }
+//     const body = rdata.body
+//     const req = http.request('http://localhost:8000' + url, { method }, (res) => {
+//         let data = ''
+//         res.on('data', (chunk) => {
+//             data += chunk
+//         })
+//         res.on('end', () => {
+//             socket.send(data)
+//         })
+//     }).on('error', (err) => {
+//         console.log(err)
+//     })
+//     req.setHeaders(headers)
+//     req.end(body)
+// })
 main()
--- server.js
+++ server.js
@@ -1,42 +1,93 @@
-import { WebSocketServer } from 'ws';
-import readline from 'node:readline/promises';
+import { WebSocket, WebSocketServer } from 'ws';
 import { createServer } from 'node:http';
-import { TransferRequestObj } from './client.js';
+import { v4 as uuidv4 } from 'uuid'
+import * as readLine from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
+import { MessageType } from './shared.js';
+import { log } from 'console'
 
-const WEB_SERVER_PORT = 8081
+const WEB_SERVER_PORT = 80
+const WEB_SOCKET_PORT = 8080
 
+const avalibleHosts = [
+    'examplee-domain.com',
+    'example-domain.com'
+]
 
-function main() {
-    const wss = new WebSocketServer({ port: 8080 });
+const messagesHandler = (data, wsId) => {
+    console.log(wsId, ':', data.toString())
+}
+
+async function main() {
+    const rl = readLine.createInterface({ input, output, prompt: '> ' })
+    const wss = new WebSocketServer({ port: WEB_SOCKET_PORT });
     const server = createServer()
+    const clients = new Map()
+    const clientsHosts = new Map()
 
     wss.on('connection', (ws, req) => {
-        console.log('new ws connection')
-        server.on('request', (req, res) => {
-            console.log('new', req.method, 'request on', req.url)
-            req.pause()
 
-            let data = ""
-            req.on('data', (chunk) => { data += chunk })
-            const tobj = new TransferRequestObj(req.url, req.method, req.headers, data)
-            ws.send(JSON.stringify(tobj), (err) => {
-                if (err) console.log(err);
-            })
-            let evdata = ''
-            ws.addEventListener('message', (ev) => {
-                evdata = ev.data
-                req.resume()
-            })
-            req.on('end', () => {
-                res.end(evdata)
-            })    
+        const clientId = uuidv4()
+        ws.id = clientId
+        clients.set(ws.id, ws)
+
+        const host = avalibleHosts.pop()
+        clientsHosts.set(host, ws.id)
+        ws.host = host
+
+        console.log('new ws connection', clientId)
+
+        ws.on('message', (data) => {
+            const messsage = JSON.parse(data.toString())
+            if (messsage.requestType === 'SYSTEM') {
+                if (messsage.request === 'AVALIBLE_HOSTS') {
+                    log('AVALIBLE_HOSTS')
+                    ws.send(new Buffer.from(JSON.stringify(avalibleHosts)))
+                }
+            } else {
+                log('USER')
+            }
         })
+
+        ws.on('close', (code, reason) => {
+            console.log('connection', ws.id, 'closed')
+            avalibleHosts.push(ws.host)
+            clients.delete(ws.id)
+        })
+    })
+
+    function createHttpReuest({method, url, headers, body}){
+        return {
+            type: MessageType.HTTP_REQUEST,
+            method,
+            url,
+            body: body || null
+        }
+    }
+
+    server.on('request', (req, res) => {
+        const host = req.headers.host
+        const ws = clients.get(clientsHosts.get(host))
+        if (ws instanceof WebSocket) {
+            const httpRequest = createHttpReuest(req.method, req.url, req.headers, req.body)
+            ws.send(new Buffer.from(JSON.stringify(httpRequest)))
+            const httpResponse = new Promise ((resolve, reject )=>{
+                ws.once('message', (data)=>{
+                    resolve(JSON.parse(data.toString()))
+                }).once('error', (error)=> {
+                    reject(error)
+                })
+            })
+            await httpResponse
+            log(httpResponse)
+        }
+        res.writeHead(200, `{Content-Type: text/plain}`)
+        res.end('OK')
     })
 
     server.listen(WEB_SERVER_PORT, () => {
         console.log('сервер слушает на порту:', WEB_SERVER_PORT)
     })
-
 
 }
 main()
